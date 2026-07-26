bd7aecf13ed1585a1a7a694420db2cfd77029038
reddot <beqiri40430@gmail.com>
2026-07-26 18:27:24 +0200

    0.2.4.0

--- components/CustomerTicketsDashboard.tsx
+++ components/CustomerTicketsDashboard.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React, { useState, useEffect } from 'react';
 import { useTranslation } from '@/lib/i18n';
 import { getTicketTranslations } from '@/lib/i18n/ticket-translations';
